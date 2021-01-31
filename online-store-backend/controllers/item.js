8bca50294a96cddb86046234f56990f392c1a3d3
pingu598 <samuel.t.jumppanen@gmail.com>
2021-01-31 11:56:20 +0200

    added functionality to cart

--- online-store-backend/controllers/item.js
+++
@@ -1,9 +0,0 @@
-const itemRouter = require('express').Router()
-
-//TODO: Item controller
-itemRouter.get('/', (request, response) => {
-    
-    response.end("test")
-})
-
-module.exports = itemRouter
